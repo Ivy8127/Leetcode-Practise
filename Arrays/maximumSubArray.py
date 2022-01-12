5bafbd66844e0ba60c6a5cb68194693fff2510d9
Ivy Mboya <mboyaivy@gmail.com>
2022-01-12 20:47:26 +0300

    kadane's algorithm revised

--- Arrays/maximumSubArray.py
+++ Arrays/maximumSubArray.py
@@ -10,13 +10,13 @@ def maxSubArray(nums):
         return ans
 
 #Kadane's algorithm O(n) -Time complexity
-def maxSubArray(arr):
-    max_sub_array = curr_max = arr[0]
-    for i in range(1,len(arr)-1):
-        curr_max = max(arr[i], max_sub_array + arr[i])
+def maxSubArray2(arr):
+    max_sub_array = curr_max = arr[0] 
+    for i in range(1,len(arr)):
+        curr_max = max(arr[i], curr_max + arr[i])
 
         if curr_max > max_sub_array:
             max_sub_array = curr_max
     return max_sub_array        
 
-print(maxSubArray([-2,2,5,-11,6]))        
+print(maxSubArray2([-2,1,-3,4,-1,2,1,-5,4]))        
