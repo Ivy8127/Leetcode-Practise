d30f08bba42367b1b304325c7c5ffeb208b7d6ca
Ivy Mboya <mboyaivy@gmail.com>
2022-01-14 11:49:36 +0300

    combination sum

---
+++ Resursion/combinationSum.py
@@ -0,0 +1,23 @@
+#Time complexity - O(n^2)
+def combinationSum(candidates, target):
+    result = []
+    combinations = []
+    candidates.sort()
+    return dfs(candidates,target,combinations,result)
+
+def dfs(candidates,target,combinations,result):
+    if target == 0:
+        result.append(combinations)
+        return
+    if target < 0:
+        return
+
+    for i in range(len(candidates)):
+        if candidates[i] > target :
+            break
+        else:
+            dfs(candidates[i:],target-candidates[i],combinations+[candidates[i]],result)
+
+    return result
+
+print(combinationSum([2,3,6,7],7))            
