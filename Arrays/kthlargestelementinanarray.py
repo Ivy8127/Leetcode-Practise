6ffb15fa73165a83208e5c78452826026031d79f
Ivy Mboya <mboyaivy@gmail.com>
2022-01-24 12:10:25 +0300

    kth largest element in an array - heap approach

--- Arrays/kthlargestelementinanarray.py
+++ Arrays/kthlargestelementinanarray.py
@@ -1,3 +1,17 @@
+#Approach 1  - Best approach using a heap ,more efficient Time complexity O(nlogk), 
+#space complexity O(k)
+import heapq as hq
+def k(nums,k):
+    heap = []
+    for num in nums:
+        hq.heappush(heap,num)    
+        if len(heap) > k:
+           hq.heappop(heap)
+    return heap[0] 
+print(k([3,2,1,5,6,4],2))            
+
+
+#Approach 2 Time complexity O(nlogn)
 def findKthLargest(nums, k):
         if len(nums) == 0:
             return 0
