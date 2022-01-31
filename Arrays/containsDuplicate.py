f2708cb3b64ea241f54bc998f8d0b6dbc14e7031
Ivy Mboya <mboyaivy@gmail.com>
2022-01-31 08:53:39 +0300

    Contains duplicates

---
+++ Arrays/containsDuplicate.py
@@ -0,0 +1,19 @@
+
+def containsDuplicate(nums):
+    num_S = set(nums)
+    if len(num_S) == len(nums):
+        return False
+    return True
+
+print(containsDuplicate([1,2,3,4,1]))   
+#or
+def cd(nums):
+    num_s = set()
+    for i in range(len(nums)):
+        if nums[i] in num_s:
+            return True
+        else:
+            num_s.add(nums[i])
+    return False 
+
+print(cd([1,2,3,4]))               
