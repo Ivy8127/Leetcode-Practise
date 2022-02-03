7fd5d6ce3e7d58dccfac163030f9a7a82e46df11
Ivy Mboya <mboyaivy@gmail.com>
2022-02-03 13:15:03 +0300

    climbing stairs

---
+++ Dynamic Programming/climbingStairs.py
@@ -0,0 +1,10 @@
+def climbStairs(n):
+        #Fibonacci series pattern
+        dp = [0] * (n+1) #0 0 0
+        dp[0] = 1 #1 1 0
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+print(climbStairs(n=2))        
