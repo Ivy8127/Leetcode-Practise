0e7374b32b55e88643208ebbc56702dc666b48cd
Ivy Mboya <mboyaivy@gmail.com>
2022-02-04 09:19:23 +0300

    jump game

---
+++ Dynamic Programming/jumpGame.py
@@ -0,0 +1,8 @@
+def jump(nums):
+    goal = len(nums)-1
+    for i in range(len(nums)-1, -1,-1):
+        if i + nums[i]>= goal:
+            goal = i
+    return True if goal == 0 else False
+
+print(jump([2,3,1,1,4]))            
