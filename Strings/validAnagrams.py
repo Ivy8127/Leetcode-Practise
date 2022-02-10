dffefad0e3f8604f78d3c4cc7bcf8412cb0ebc83
Ivy Mboya <mboyaivy@gmail.com>
2022-02-10 10:12:39 +0300

    valid anagrams - hashmap approach

--- Strings/validAnagrams.py
+++ Strings/validAnagrams.py
@@ -4,5 +4,28 @@ def validAnagrams(s,t):
     if s==t:
         return True
     return False
-print(validAnagrams("nat","bat"))#False        
+print(validAnagrams("nat","bat"))#False 
+
+
+#Approach 2 - hashmaps
+def validA(s,t):
+    anagrams = {}
+    #populate a hashmap with string s characters
+    for char in s:
+        if char in anagrams:
+            anagrams[char]+=1
+        else:
+            anagrams[char] = 1
+
+    for char in t:
+        if char in anagrams:
+            anagrams[char] -= 1
+        else:
+            return False #char is not an anagram bc it's not contained in hashmap
+    for k,v in anagrams.items():
+        if v == 0: #matching charater numbers
+            return True  
+    return False                  
+
 print(validAnagrams("nat","tan"))#True        
+
