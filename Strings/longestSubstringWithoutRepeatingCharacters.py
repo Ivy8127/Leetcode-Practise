40745c21decbdbde206e8d905a4a2c2731218229
Ivy Mboya <mboyaivy@gmail.com>
2022-02-12 10:33:37 +0300

    longest substring without repeating characters

---
+++ Strings/longestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,15 @@
+#Sliding window + set technique
+def lengthOfLongestSubstring( s):
+    res = 0
+    left = 0
+    charSet = set()
+    
+    for right in range(len(s)):
+        while s[right] in charSet:
+            charSet.remove(s[left])
+            left +=1
+            
+        charSet.add(s[right])
+        res = max(res, len(charSet))
+    return res    
+print(lengthOfLongestSubstring("pwwkew")) #3        
