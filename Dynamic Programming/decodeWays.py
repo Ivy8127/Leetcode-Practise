49c1fb264c5b556e3ad2dcc44f7ab002f2d5c6e3
Ivy Mboya <mboyaivy@gmail.com>
2022-02-16 11:10:43 +0300

    decode ways

--- Dynamic Programming/decodeWays.py
+++ Dynamic Programming/decodeWays.py
@@ -1,13 +1,31 @@
-
 def numDecodings(s):
-    dp = {len(s):1}
-    for i in range(len(s)-1,-1,-1):
-        if s[i] == "0":
-            dp[i] = 0
+    dp = [0] * (len(s)+1)
+    dp[0] =1 #accomodating an empty string
+    
+    #base case
+    if s[0] == '0':
+        return 0
+    else:
+        dp[1] = 1 #if the first string character is not a 0, you can only decode it once
+        
+    for i in range(2, len(s)+1) :
+        #4cases
+        #Case 3 : value at i-1 is 0 and 0-2 is 1 or 2 meaning that it's within 26
+        #- decode in 1 way - taking the whole number as 1 way eg 20
+        if s[i-1] == '0':
+            if s[i-2] == '1' or s[i-2] == '2':
+                dp[i] = dp[i-2]
+            else:
+                #case 4 , number is past 26 therefore can't be decoded .eg 60
+                return 0 
         else:
-            dp[i] = dp[i + 1]
-            
-        if (i+1 < len(s) and (s[i] == "1" or s[i]=="2" and s[i+1] in "0123456")):
-            dp[i] += dp[i+2]
-    return dp[0]  
-print(numDecodings("122"))       
+            #case 1 : value between 10 and 26 - decode in 2 ways
+            if s[i-2] == "1" or s[i-2] == "2" and int(s[i-1]) < 7:
+                dp[i] = dp[i-1] + dp[i-2]
+            else:
+                #case 2 : value is greater than 26 - decode in 1 way
+                dp[i] = dp[i-1]
+    return dp[len(s)]            
+                
+print(numDecodings("12")) # 1, 2 or 12 -> can be decoded in 2 ways #2                
+        
