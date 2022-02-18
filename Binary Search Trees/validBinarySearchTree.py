f8fa7e9a5859ac2af4fdf626c49873a7615b2a7c
Ivy Mboya <mboyaivy@gmail.com>
2022-02-18 09:17:52 +0300

    valid bst

---
+++ Binary Search Trees/validBinarySearchTree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        #using left and right boundaries to determine a valid bst
+        
+        def valid(node, left, right):
+            if not node:
+                return True #bc an empty node is a valid bst
+            
+            #the node value should be greater than the left subtree and less than the right subtree to be a valid bst
+            if not (node.val > left and node.val < right):
+                return False
+            
+            return (valid(node.left, left, node.val) and valid(node.right, node.val,right))
+        
+        return valid(root, float('-inf'), float('inf'))
+        
+        
