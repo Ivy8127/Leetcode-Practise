12e1b936a9dffb242b5ec2921d5bba1bca2711e8
Ivy Mboya <mboyaivy@gmail.com>
2022-02-21 13:30:51 +0300

    path sum intuitive solution

--- Binary Search Trees/pathSum.py
+++ Binary Search Trees/pathSum.py
@@ -16,3 +16,27 @@ class Solution:
             return True
         
         return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
+
+#More intuitive solution for me
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, currSum):
+            if node is None:
+                return False
+            currSum += node.val
+            if node.left == None and node.right == None:
+                if currSum == targetSum:
+                    return True
+                else:
+                    return False
+                
+            return (dfs(node.left, currSum) or dfs(node.right, currSum)) 
+        
+        return dfs(root,0)
+                    
