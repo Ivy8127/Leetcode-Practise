8f82b45326f9039e675a03bd2f3c9ffdfff93698
Ivy Mboya <mboyaivy@gmail.com>
2022-02-21 09:16:15 +0300

    maximum swap changes

--- Arrays/maximumSwap.py
+++ Arrays/maximumSwap.py
@@ -20,21 +20,21 @@ def maximumSwap(self, num: int) -> int:
 
 #This the bruteforce solution O(n^2)
 def maximumSwap(num):
-max_num = num
-#num is an integer, convert it to a list
+    max_num = num
+    #num is an integer, convert it to a list
 
-s= list(str(num)) # ['2','7','3','6']
+    s= list(str(num)) # ['2','7','3','6']
 
-for i in range(len(s)):
-    for j in range(i+1, len(s)):
-        #swap
-        s[i] , s[j]= s[j], s[i]
-        
-        max_num = max(max_num, int(''.join(s)))
-        
-        #return to original num for swapping again
-        #by reswapping
-        s[i], s[j] = s[j], s[i]
-return max_num         
+    for i in range(len(s)):
+        for j in range(i+1, len(s)):
+            #swap
+            s[i] , s[j]= s[j], s[i]
+            
+            max_num = max(max_num, int(''.join(s)))
+            
+            #return to original num for swapping again
+            #by reswapping
+            s[i], s[j] = s[j], s[i]
+    return max_num         
             
             
