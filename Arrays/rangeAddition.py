dbb3fcdbbccdb9640932ca1db555dd47dfda3635
Ivy Mboya <mboyaivy@gmail.com>
2022-03-01 17:39:14 +0300

    amazon oa

--- Arrays/rangeAddition.py
+++ Arrays/rangeAddition.py
@@ -13,4 +13,27 @@ def rangeAddition(length,updates):
     for i in range(1, len(res)):
         res[i] = res[i] + res[i-1]
     return res
-print(rangeAddition(5, [[1,3,2],[2,4,3],[0,2,-2]]))               
+print(rangeAddition(5, [[1,3,2],[2,4,3],[0,2,-2]]))  
+
+
+def wifiRouters(buildingCount, routerLocation, routerRange):
+    n = len(buildingCount)
+    routerCount = [0]*n
+    
+    for i in range(len(routerLocation)):
+        rng = routerRange[i]
+        startIdx = max(0, routerLocation[i]-1-rng)
+        endIdx = min(n-1, routerLocation[i]-1+rng)
+        
+        routerCount[startIdx] += 1
+        if endIdx < n-1:
+            routerCount[endIdx+1] -= 1
+    
+    for i in range(1, n):
+        routerCount[i] += routerCount[i-1]
+    
+    count = 0
+    for i in range(n):
+        if routerCount[i] >= buildingCount[i]:
+            count += 1
+    return count 
