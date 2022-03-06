0207b919ee08cda5913b7291bb5c461544a792af
Ivy Mboya <mboyaivy@gmail.com>
2022-03-06 20:32:26 +0300

    valid palindrome II

--- Strings/validPalindrome2.py
+++ Strings/validPalindrome2.py
@@ -1,17 +1,23 @@
 #Time complexity O(n)
 #using 2 pointer technique
-class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        if len(s) < 2:
-            return True
-        left = 0
-        right = len(s)-1
-        while left < right:
-            if s[left] == s[right]:
-                left+= 1
-                right -=1
-            else:
-                #delete the left and right pointer
-                return s[left+1:right+1] == s[left+1:right+1][::-1] or s[left:right] == s[left:right][::-1]
-        return True        
+
+def validPalindrome( s):
+    if len(s) < 2:
+        return True
+    left = 0
+    right = len(s)-1
+    while left < right:
+        if s[left] == s[right]:
+            left+= 1
+            right -=1
+        else:
+            #delete the left and right pointer to check whether it is a palindrome
+            return s[left+1:right+1]
+    return True 
+print(validPalindrome('abca'))   
+# a b c a
+#   s e 
+# left -? right -> b => b leaving  a c a -> return true bc it is a palindrome when reversed
+# left + 1 right + 1 => c leaving a b a -> returns true bc it is a palindrome when reversed
+        
         
