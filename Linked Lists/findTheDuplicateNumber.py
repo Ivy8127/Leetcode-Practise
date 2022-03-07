32637b4cc239712c7a8584baab0addec10aeb94a
Ivy Mboya <mboyaivy@gmail.com>
2022-03-07 13:58:57 +0300

    find the duplicate number

---
+++ Linked Lists/findTheDuplicateNumber.py
@@ -0,0 +1,18 @@
+#Using Floyd's Cycle detection algoritm to solve the question in constant space
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = 0
+        fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2= nums[slow2]
+            if slow == slow2:
+                return slow
+        
