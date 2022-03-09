1fed5425e3fe1e210f12a9bb6e19e7dc1a732cd3
Ivy Mboya <mboyaivy@gmail.com>
2022-03-09 15:14:48 +0300

    maximum average subarray i

--- Sliding Window Pattern/maximum_Average_subaArray1.py
+++ Sliding Window Pattern/maximum_Average_subaArray1.py
@@ -1,5 +1,16 @@
-def max_avg_subarray(arr,k):
-    
-
-print(max_avg_subarray([10,30,20,50,60,40,40], 2))          
-
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        #sliding window approach
+        #idea is to remove the left most value an adding the new
+        #right most value into the window
+        #step 1 calculate first window
+        res = sum(nums[:k])
+        max_res = res
+        for i in range(len(nums)-k):
+            #delete first element from window an add next element into it
+            res = res - nums[i] + nums[i+k]
+            max_res = max(max_res, res)
+        return max_res/k    
+            
+        
+        
