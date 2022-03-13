80777d8638095e5c555866082f22d54793d895d1
Ivy Mboya <mboyaivy@gmail.com>
2022-03-13 23:04:10 +0300

    best time to buy and sell stock

--- Arrays/bestTimetoBuyandsellStock.py
+++ Arrays/bestTimetoBuyandsellStock.py
@@ -1,13 +1,16 @@
 def maxProfit(prices) -> int:
+#return 0 bc there is no element in arary
     if len(prices) == 0:
         return 0
     curr_min = prices[0]
     max_profit = 0
     for i in range(1,len(prices)):
+    #get the minimum value in array as it promises the maximum profit ever
         if prices[i] < curr_min:
             curr_min = prices[i]
         elif prices[i] - curr_min > max_profit:
             max_profit = prices[i] - curr_min
+
     return max_profit
 
 
