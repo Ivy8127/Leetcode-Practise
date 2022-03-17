4f4a5c38471d855ddcbf6d17cf06d3460a0c6a67
Ivy Mboya <mboyaivy@gmail.com>
2022-03-17 21:51:44 +0300

    valid parenthesis

--- Stack/validparenthesis.py
+++ Stack/validparenthesis.py
@@ -41,5 +41,22 @@ def validP(s):
 
 print(validP("{ ( [] ) }"))
 
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == "(" or char == "[" or char == "{":
+                stack.append(char)
+            elif char == ")" and stack and stack[-1] == "(":
+                stack.pop()
+            elif char == "]" and stack and stack[-1] == "[":
+                stack.pop()
+            elif char == "}" and stack and stack[-1] == "{":
+                stack.pop()    
+            else:
+                return False
+        if not stack:
+            return True
+        else:
+            return False
 
