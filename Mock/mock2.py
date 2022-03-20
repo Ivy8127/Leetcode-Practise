d0ca20a9210e821b5c7d14c53a315d167fda5ce6
Ivy Mboya <mboyaivy@gmail.com>
2022-03-20 23:12:15 +0300

    mock interview 2

---
+++ Mock/mock2.py
@@ -0,0 +1,16 @@
+
+#problem  n = 3
+#melborp
+#m e l p r o b
+#melprob
+#word n = 1 len(str) - n
+#dwor
+
+def rotateArray(arr, n):
+    res = []
+    for i in range(len(arr)-1, len(arr)-1 -n,-1):
+
+# 7 8 9 11 12 
+# 1 range len(arr) + 1  6
+
+
