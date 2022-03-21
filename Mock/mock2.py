acdfbb8a3431dbaeb286e050d34cd4135ad24ff7
Ivy Mboya <mboyaivy@gmail.com>
2022-03-21 16:56:06 +0300

    mock 2

--- Mock/mock2.py
+++ Mock/mock2.py
@@ -10,7 +10,6 @@ def rotateArray(arr, n):
     res = []
     for i in range(len(arr)-1, len(arr)-1 -n,-1):
 
-# 7 8 9 11 12 
-# 1 range len(arr) + 1  6
+#fnd the missing positive integer - working on this all night today, don't care
 
 
