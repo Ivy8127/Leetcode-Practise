110e34fc9a03914e2013cd8e47569c4ea69b61a0
Ivy Mboya <mboyaivy@gmail.com>
2022-03-25 11:49:16 +0300

    Number of islands

---
+++ Graphs/numberofIslands.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        visited = set()
+        rows = len(grid)
+        cols = len(grid[0])
+        def bfs(r,c):
+            q = deque()
+            q.append([r,c])
+            visited.add((r,c))
+            while q:
+                r,c = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    row = dr + r
+                    col = dc+c
+                    if (row in range(rows) and col in range(cols) and grid[row][col] == "1" and (row,col) not in visited):
+                        q.append([row,col])
+                        visited.add((row,col))
+                
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands+=1
+        return islands            
