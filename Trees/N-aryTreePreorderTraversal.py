33e23c78ee6617cceb21fd5498ea816b232765c0
Ivy Mboya <mboyaivy@gmail.com>
2022-03-27 20:21:55 +0300

    Tree questions

---
+++ Trees/N-aryTreePreorderTraversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        res = []
+        def dfs(node):
+            if node != None:
+                res.append(node.val)
+                for n in node.children:
+                    dfs(n)
+        dfs(root)
+        return res
+        
