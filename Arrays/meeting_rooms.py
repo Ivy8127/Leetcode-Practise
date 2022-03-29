2a8f8fc8de42970bb2ab0c63ef543ccdb52ba857
Ivy Mboya <mboyaivy@gmail.com>
2022-03-29 20:40:15 +0300

    meeting rooms I

--- Arrays/meeting_rooms.py
+++ Arrays/meeting_rooms.py
@@ -23,3 +23,10 @@ def canAttendMeetings(intervals):
         if intervals[i][0] < intervals[i-1][1]:
             return False
     return True 
+
+def canAttendMeetings(intervals):
+    intervals.sort() 
+    for i in range(1,len(intervals)):
+        if intervals[i][0] < intervals[i-1][1]:
+            return False
+    return True    
