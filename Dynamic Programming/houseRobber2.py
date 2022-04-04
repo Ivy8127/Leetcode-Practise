09b8fa07d67fa48ee527ebc39a9814a4c5ce278e
Ivy Mboya <mboyaivy@gmail.com>
2022-04-04 11:08:08 +0300

    house robber 2 - revised

--- Dynamic Programming/houseRobber2.py
+++ Dynamic Programming/houseRobber2.py
@@ -1,6 +1,31 @@
 #Same approach as house robber 1, only difference is to create 2 subarrays
 #One includes the first house without the last and the other includes the last house without the first
 #bc they're adjacent to each other in a circle
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.rob1(nums[:-1]), self.rob1(nums[1:]))
+    
+    def rob1(self, nums):
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+
+        dp = nums[:]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2]+ dp[i])
+        return dp[len(nums)-1]
+
+
+
+
+
 def rob(nums):
     return max(nums[0],helper(nums[:-1]),helper(nums[1:]))
     
