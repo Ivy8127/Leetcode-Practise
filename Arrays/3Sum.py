0298ad2f39a194f2c5f31ebe636f48e184db7df6
Ivy Mboya <mboyaivy@gmail.com>
2022-04-20 20:37:25 +0300

    3sum

--- Arrays/3Sum.py
+++ Arrays/3Sum.py
@@ -24,5 +24,35 @@ def threeSum(nums):
 
 print(threeSum([-1,0,1,2,-1,-4]))                  
 print(threeSum([-1,0,1,2,-1,-4]))                  
-print(threeSum([-1,0,1,2,-1,-4]))                  
+print(threeSum([-1,0,1,2,-1,-4])) 
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        #if arr is empty return []
+        # -1 0 1 2 -1 -4
+        #sort array
+        # -4 -1 -1 0 1 2
+        
+        if len(nums)==0:
+            return []
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            #checking for duplicates
+            if i > 0 and nums[i] == nums[i-1] :
+                continue
+            left, right = i +1 , len(nums)-1    
+            while  left < right:
+                t_sum = nums[i] + nums[left] + nums[right]
+                if t_sum < 0:
+                    left +=1
+                elif t_sum > 0:
+                    right -=1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left+=1
+                    #check for duplicates in the pointer values
+                    while left < right and nums[left] == nums[left-1]:
+                        left +=1
+        return res                
+                             
 
